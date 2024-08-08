9d2f1f894ca59f133e0ede2990c946c48f7243a3
Amisha-M <amishamadanu@gmail.com>
2024-08-08 17:41:00 +0530

    Implemented Jaxb Context for conversion in service layer

--- src/main/java/com/ems/service/EmployeeServiceImpl.java
+++ src/main/java/com/ems/service/EmployeeServiceImpl.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import com.ems.util.JaxbUtil;
+import jakarta.xml.bind.JAXBException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,15 +23,6 @@ public class EmployeeServiceImpl implements EmployeeService{
 
 	private static final Logger logger = LoggerFactory.getLogger(EmployeeService.class);
 
-//    @Autowired
-//    private EmployeeRepository employeeRepository;
-
-//	@Autowired
-//	private EmployeeBO employeeBO;
-//	
-//	private final EmployeeMapper employeeMapper = EmployeeMapper.INSTANCE;
-	
-    
 	private final EmployeeBO employeeBO;
     private final EmployeeMapper employeeMapper;
 
@@ -45,7 +38,24 @@ public class EmployeeServiceImpl implements EmployeeService{
     	 logger.info(Constants.CREATING_EMPLOYEE_LOG, employeeVO);
          Employee employee = employeeMapper.employeeVOToEmployee(employeeVO);
          Employee createdEmployee = employeeBO.createEmployee(employee);
-         return employeeMapper.employeeToEmployeeVO(createdEmployee);
+         EmployeeVO createdEmployeeVO = employeeMapper.employeeToEmployeeVO(createdEmployee);
+
+//        return employeeMapper.employeeToEmployeeVO(createdEmployee);
+
+        try {
+            // Marshalling(write) to JSON
+            String json = JaxbUtil.toJson(createdEmployeeVO);
+            logger.info("Created Employee JSON: {}", json);
+
+            // Unmarshalling(read) from JSON
+            EmployeeVO unmarshalledEmployeeVO = JaxbUtil.fromJson(json, EmployeeVO.class);
+            logger.info("Unmarshalled EmployeeVO: {}", unmarshalledEmployeeVO);
+
+        } catch (JAXBException e) {
+            logger.error("Error in JSON Conversion", e);
+        }
+
+        return createdEmployeeVO;
      }
 
 	@Override
@@ -61,7 +71,22 @@ public class EmployeeServiceImpl implements EmployeeService{
     public Optional<EmployeeVO> getEmployeeById(Long id) {
 		logger.info(Constants.RETRIEVING_EMPLOYEE_BY_ID_LOG, id);
         Optional<Employee> employee = employeeBO.getEmployeeById(id);
-        return employee.map(employeeMapper::employeeToEmployeeVO);
+//        return employee.map(employeeMapper::employeeToEmployeeVO);
+        return employee.map(emp -> {
+            EmployeeVO employeeVO = employeeMapper.employeeToEmployeeVO(emp);
+            try {
+                String json = JaxbUtil.toJson(employeeVO);
+                logger.info("Retrieved Employee JSON: {}", json);
+
+                // Unmarshalling(read) from JSON
+                EmployeeVO unmarshalledEmployeeVO = JaxbUtil.fromJson(json, EmployeeVO.class);
+                logger.info("Unmarshalled EmployeeVO: {}", unmarshalledEmployeeVO);
+
+            } catch (JAXBException e) {
+                logger.error("Error in JSON Conversion", e);
+            }
+            return employeeVO;
+        });
     }
 	
 }
